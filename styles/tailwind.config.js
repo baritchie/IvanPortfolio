defb5b43f5bc131bf73a154655db73c0dcff298e
Benjamin Rojas-Ritchie <ben@benarr.com>
2021-09-28 16:22:35 -0600

    feat: commit experience/nav/about/styling

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,18 +1,29 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: ['_site/**/*.html'],
-    options: {
-      safelist: [],
+    mode: 'jit',
+    purge: {
+        content: ['_site/**/*.html'],
+        options: {
+            safelist: [],
+        },
     },
-  },
-  theme: {
-    extend: {
-      colors: {
-        change: 'black',
-      },
+    theme: {
+        extend: {
+            colors: {
+                darkblue: '#124559',
+                lightblue: '#598392',
+                green: '#AEC3B0',
+                lightgreen: '#EFF6E0',
+            },
+            fontFamily: {
+                quat: ['Quattrocento', 'serif'],
+                quatsans: ['Quattrocento Sans', 'sans-serif'],
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ['odd', 'even'],
+        },
+    },
+    plugins: [],
 }
